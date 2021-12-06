e901c25eb5a255b3f3701abc00154cee5fe8706e
UNREALBOT12 <91039807+UNREALBOT12@users.noreply.github.com>
2021-12-06 17:34:29 +0530

    Update pmpermit.py

--- program/pmpermit.py
+++ program/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"𝗧𝗵𝗶𝘀 𝗜𝘀 𝗕𝗼𝘁 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝗰𝗲 𝗝𝗼𝗶𝗻 @eSportClan 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽")
+  await USER.send_message(message.chat.id,"🔰ᴛʜɪs ɪs ᴀssɪsᴛᴀɴᴛ ᴏғ @FallenMusicBot ᴊᴏɪɴ @TheCreatorPavan ғᴏʀ ᴜᴘᴅᴀᴛᴇᴅ🔰")
   return
