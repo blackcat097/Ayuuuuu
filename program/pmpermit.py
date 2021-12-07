5c87d40789f01d48c41530c8fadcf7412cad6076
Kanna72 <92355529+blackcat097@users.noreply.github.com>
2021-12-07 23:31:07 +0530

    Update pmpermit.py

--- program/pmpermit.py
+++ program/pmpermit.py
@@ -6,5 +6,5 @@ from pyrogram.types import Chat, Message, User
 
 @Client.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"🔰ᴛʜɪs ɪs ᴀssɪsᴛᴀɴᴛ ᴏғ @FallenMusicBot ᴊᴏɪɴ @TheCreatorPavan ғᴏʀ ᴜᴘᴅᴀᴛᴇᴅ🔰")
+  await USER.send_message(message.chat.id,"🔰ᴛʜɪs ɪs ᴀssɪsᴛᴀɴᴛ ᴏғ @ammuXmusicbot ᴊᴏɪɴ @catmusicworld ғᴏʀ ᴜᴘᴅᴀᴛᴇᴅ🔰")
   return
